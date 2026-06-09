c0b770f94ed482e5db135b4dc92ca181aa9e9f5e
someDev01 <dmitryprogger@mail.ru>
2026-06-10 00:48:22 +0300

    set email from

--- backend/Online_KinoTeater/Infrastructure/Services/Email/Smtp/ResendEmailService.cs
+++ backend/Online_KinoTeater/Infrastructure/Services/Email/Smtp/ResendEmailService.cs
@@ -1,15 +1,19 @@
 ﻿using System.Net.Http.Json;
 using Application.Interfaces.Email;
+using Infrastructure.Settings.Email;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Services.Email.Smtp;
 
-public class ResendEmailService(HttpClient client) : IEmailService
+public class ResendEmailService(
+    HttpClient client,
+    IOptions<EmailSettings> options) : IEmailService
 {
     public async Task SendAsync(string to, string topic, string body, CancellationToken cancellationToken)
     {
         var response = new
         {
-            from = "onboarding@resend.dev",
+            from = options.Value.From,
             to,
             subject = topic,
             html = body
