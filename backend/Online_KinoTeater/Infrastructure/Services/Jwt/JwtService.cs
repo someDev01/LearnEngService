0a0f2a1a995c17fa2a008d04b090f25d3ef303be
someDev01 <dmitryprogger@mail.ru>
2026-06-17 14:14:46 +0300

    l

--- backend/Online_KinoTeater/Infrastructure/Services/Jwt/JwtService.cs
+++ backend/Online_KinoTeater/Infrastructure/Services/Jwt/JwtService.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Domain.Model.Entyties;
 
 namespace Infrastructure.Services.Jwt;
 
