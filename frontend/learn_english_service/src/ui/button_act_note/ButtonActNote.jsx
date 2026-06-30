a8f6e1f2098c13f245464ccf7621ae1c95590b42
someDev01 <dmitryprogger@mail.ru>
2026-06-30 13:10:22 +0300

    l

--- frontend/learn_english_service/src/ui/button_act_note/ButtonActNote.jsx
+++ frontend/learn_english_service/src/ui/button_act_note/ButtonActNote.jsx
@@ -4,9 +4,9 @@ import styles from '../button_act_note/button_act_note.module.css';
 function ButtonActNote({type, onClick}){
     return(
         <div className={`${styles.button_act} ${styles[type]}`} onClick={onClick}>
-            {type === "view" && <p><Eye size={18} color='#ffc680'/></p>}
-            {type === "edit" && <p><Edit2 size={18} color='#ff7700'/></p>}
-            {type === "delete" && <p><Trash2 size={18} color='red'/></p>}
+            {type === "view" && <Eye size={16} color='#edb673'/>}
+            {type === "edit" && <Edit2 size={16} color='#ec6e00'/>}
+            {type === "delete" && <Trash2 size={16} color='#e00000'/>}
         </div>
     )
 }
