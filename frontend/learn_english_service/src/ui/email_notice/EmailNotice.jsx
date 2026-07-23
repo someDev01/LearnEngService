34f19af8555a70a3ee0e2236a321e221930eae00
someDev01 <dmitryprogger@mail.ru>
2026-07-23 19:09:52 +0300

    changeForms

--- frontend/learn_english_service/src/ui/email_notice/EmailNotice.jsx
+++ frontend/learn_english_service/src/ui/email_notice/EmailNotice.jsx
@@ -1,9 +1,10 @@
 import { LockKeyholeIcon } from "lucide-react";
 import styles from '../email_notice/email_notice.module.css';
 
+
 function EmailNotice(){
     return(
-        <small><LockKeyholeIcon size={13} color='#a9a9a9'/> Почта используется только для входа</small>
+        <div className={styles.email_notice}><LockKeyholeIcon size={13} color='#a9a9a9'/> Почта используется только для входа</div>
     )
 }
 
